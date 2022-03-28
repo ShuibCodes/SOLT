71fcf709f8c4256d2e16d46bcbff15b471552c9d
Shuib Abdillahi <71759511+ShuibCodes@users.noreply.github.com>
2022-03-28 10:49:50 +0100

    React tictactoe game + winner modal popup

--- src/App.js
+++ src/App.js
@@ -1,48 +1,14 @@
+/* eslint-disable */
 
- /* eslint-disable */ 
- import React, { useEffect, useState } from "react";
-import logo from './logo.png';
-import Tickets from './Tickets.js'
-import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
-import Loader from "react-loader-spinner";
+import React, { useState } from 'react';
+
+
+
+import TicTacToe from '../src/TicTacToe'
 function App() {
-
-
-  const [plays, setPlays] = useState([])
-
-
-
-       
-    useEffect(() => {
-      fetch('https://officiallondontheatre.com/wp-json/shows/all-open')
-        .then(res => {
-          return res.json();
-        })
-        .then(data => {
-          // only needed first 4 plays 
-          setPlays(data.slice(0,4));
-          
-        })
-    }, [])
- 
-     console.log(plays)
-
-  return (
-    <div className="h-screen flex justify-center items-center">
-
-        {/* added a loading animation because why not? */}
-
-        { plays.length === 0 ? 
-          <div>
-          <Loader type="Bars" color="red" height={90} width={90} />
-          </div>
-          
-          :
-        <Tickets plays={plays} />
-        }
-        
-  </div>
-  );
+	return <div>
+ <TicTacToe />
+  </div>;
 }
 
 export default App;
